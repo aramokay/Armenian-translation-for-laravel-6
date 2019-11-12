01dec0dc640b32c689efd1cf33b30d40e7950c2f
Aram <aramokay@gmail.com>
2019-11-12 13:31:55 +0400

    Files

---
+++ hy/translations.php
@@ -0,0 +1,73 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'phone_number'           => 'Phone number',
+    'address'                => 'Address',
+    'archive'                => 'Archive',
+    'email'                  => 'Email',
+    'actions'                => 'Actions',
+    'change_language'        => 'Change Language',
+    'board'                  => 'Board',
+    'single_board'           => [
+        'product_title'           => 'Product title',
+        'product_description'     => 'Product description',
+        'product_price'           => 'Product price',
+        'add_product'             => 'Add product',
+        'remove_product'          => 'Remove product',
+        'remove_product_question' => 'Do you want to remove this product?',
+        'change_board_name'       => 'Change board name',
+        'board_name'              => 'Board name:'
+    ],
+    'create_new_boards'      => 'Create new boards',
+    'new_language'           => 'Language name...',
+    'add_product_language'   => 'Save language',
+    'submit'                 => 'Submit',
+    'view_order_items'       => 'View order items',
+    'more_item_details'      => 'More details',
+    'item_details'           => 'Item details',
+    'orders'                 => 'Orders',
+    'order_items'            => 'Order items',
+    'total_price'            => 'Total price',
+    'dashboard'              => 'Dashboard',
+    'cart'                   => 'Cart',
+    'add_to_cart'            => 'Add to cart',
+    'complete_order'         => 'Complete order',
+    'product_count'          => 'Product count',
+    'empty_shelf'            => 'Shelf is empty',
+    'yes'                    => 'Yes',
+    'no'                     => 'No',
+    'close'                  => 'Close',
+    'save'                   => 'Save',
+    'order_confirmation_msg' => 'Your order is successfully made. Check email for details.',
+    'single_cart'            => [
+        'title'               => 'Title',
+        'qty'                 => 'Qty',
+        'price'               => 'Price',
+        'description'         => 'Description',
+        'remove'              => 'Remove',
+        'checkout'            => 'Checkout',
+        'empty_cart'          => 'Your cart is empty',
+        'remove_product'      => 'Remove product',
+        'remove_confirmation' => 'Do you want to remove this product from you cart?',
+    ],
+    'admin'                  => [
+        'download_products'  => 'Download products',
+        'upload_products'    => 'Upload products',
+        'upload_board_image' => 'Upload board image',
+        'remove_board'       => 'Remove Board',
+        'change_board_name'  => 'Change board name',
+
+    ]
+];
